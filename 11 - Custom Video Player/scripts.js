527574a19934e7d1c373a5a91c06b9e84c2f2d06
Marco Lussetti <marco@marcolussetti.com>
2018-01-29 22:34:31 -0800

    11 - Fullscreen WIP

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+const fullscreen = player.querySelector('.fullscreenbtn');
 
 
 // Build function
@@ -59,7 +60,35 @@ ranges.forEach(range => range.addEventListener('mouseover', handleRangeUpdate));
 video.addEventListener('timeupdate', handleProgress);
 
 progress.addEventListener('click', scrub);
+
 let mouseDown = false;
 progressBar.addEventListener('mousedown', () => mouseDown = true);
 progressBar.addEventListener('mouseup', () => mouseDown = false);
 progress.addEventListener('mousemove', e => mouseDown && scrub(e)); // lazy evaluation trick
+
+// Fullscreen
+let fullscreenFunction;
+let fullscreenElement;
+let exitFullscreenFunction;
+let fullscreenFunctions = ['requestFullscreen', 'mozRequestFullScreen', 'msRequestFullscreen', 'webkitRequestFullScreen'];
+let exitFullscreenFunctions = ['exitFullscreen', 'mozCancelFullScreen', 'msExitFullscreen', 'webkitExitFullscreen'];
+let fullscreenElements  = ['fullscreenElement', 'mozFullScreenElement', 'msFullscreenElement', 'webkitFullscreenElement'];
+
+fullscreenFunctions.forEach(function (tentativeFunction){
+    if (player[tentativeFunction]) {
+        index = fullscreenFunctions.indexOf(tentativeFunction);
+        fullscreenFunction = player[tentativeFunction];
+        exitFullscreenFunction = player[exitFullscreenFunctions[index]];
+        fullscreenElement = player[fullscreenElements[index]];
+        break; //if we already have a working one a working one...
+    }
+});
+
+function handleFullscreen() {
+    console.log(window[fullscreenElement]);
+    /*if (window[fullscreenElement])*/
+
+    fullscreenFunction.call(player);
+}
+
+fullscreen.addEventListener('click', handleFullscreen);
